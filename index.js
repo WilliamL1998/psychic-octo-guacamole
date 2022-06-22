df0a5396abb61a4a9ecf88cf7e1732f9fa6a2bad
WilliamL1998 <williamliao1998@gmail.com>
2022-06-22 00:06:21 -0400

    Added logic

--- index.js
+++ index.js
@@ -53,27 +53,186 @@ const promptMenu = () => {
 }
 
 const viewDepartments = () => {
-    db.query("SELECT * FROM department;", function (err, results) {
+    db.query(`SELECT * FROM department;`, function (err, results) {
         console.table(results);
         promptMenu();
     })
 }
 
 const viewRoles = () => {
-    db.query("SELECT * FROM role;", function (err, results) {
+    db.query(`SELECT role.id, role.title, department.name  AS department, role.salary
+    FROM role
+    JOIN department ON role.department_id = department.id;`, function (err, results) {
         console.table(results);
         promptMenu();
     })
 }
 
 const viewEmployees = () => {
-    db.query("SELECT * FROM employee;", function (err, results) {
+    db.query(`SELECT A.id, A.first_name, A.last_name, role.title, department.name AS department, role.salary, CONCAT(B.first_name, " ", B.last_name) AS manager
+    FROM employee A
+    JOIN role ON A.role_id = role.id
+    JOIN department ON role.department_id = department.id
+    LEFT JOIN employee B ON A.manager_id = B.id;`, function (err, results) {
         console.table(results);
         promptMenu();
     })
 }
 
 const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the name of the department?",
+                name: "department",
+                validate: (department) => {
+                    if (department) {
+                        return true;
+                    } else {
+                        console.log("Please enter the name of the department you would like to add.");
+                        return false;
+                    }
+                }
+            }
+        ])
+        .then(answers => {
+            const addThis = JSON.stringify(answers.department)
+            db.query(`INSERT INTO department (name) VALUES (${addThis})`)
+            console.log(`Added ${addThis} to the database.`)
+            promptMenu();
+        })
+}
+
+const addRole = () => {
+    db.query(`SELECT name, id FROM department`, function(err, results) {
+        let departmentChoices = results.map(({name, id}) => ({name: name, value: id}))
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the name of the role?",
+                    name: "title",
+                    validate: (title) => {
+                        if (title) {
+                            return true;
+                        } else {
+                            console.log("Please enter the name of the role you would like to add.");
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    message: "What is the salary of the role?",
+                    name: "salary",
+                    validate: (salary) => {
+                        if (salary) {
+                            return true;
+                        } else {
+                            console.log("Please enter the salary of the role.");
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "list",
+                    message: "What department does the role belong to?",
+                    choices: departmentChoices,
+                    name: "department_id",
+                    validate: (department_id) => {
+                        if (department_id) {
+                            return true;
+                        } else {
+                            console.log("Please select the name of the department the role belongs to.");
+                            return false;
+                        }
+                    }
+                }
+            ])
+            .then(({title, salary, department_id}) => {
+                db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${title}", ${salary}, ${department_id})`)
+                console.log(`Added ${title} to the database.`)
+                promptMenu();
+            })
+    })
+}
+
+const addEmployee = () => {
+    db.query(`SELECT title, id FROM role`, function(err, results) {
+        let roleChoices = results.map(({title, id}) => ({name: title, value: id}))
+
+        db.query(`SELECT CONCAT(first_name, " ", last_name) AS manager, id FROM employee`, function(err, results) {
+            let managerChoices = results.map(({manager, id}) => ({name: manager, value: id}))
+
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What is the employee's first name?",
+                        name: "first_name",
+                        validate: (first_name) => {
+                            if (first_name) {
+                                return true;
+                            } else {
+                                console.log("Please enter the first name of the employee you wish to add.");
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "input",
+                        message: "What is the employee's last name?",
+                        name: "last_name",
+                        validate: (last_name) => {
+                            if (last_name) {
+                                return true;
+                            } else {
+                                console.log("Please enter the last name of the employee you wish to add.");
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "list",
+                        message: "What is the employee's role?",
+                        choices: roleChoices,
+                        name: "role_id",
+                        validate: (role_id) => {
+                            if (role_id) {
+                                return true;
+                            } else {
+                                console.log("Please select the role of the employee you wish to add.");
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: "list",
+                        message: "Who is the employee's manager?",
+                        choices: managerChoices,
+                        name: "manager_id",
+                        validate: (manager_id) => {
+                            if (manager_id) {
+                                return true;
+                            } else {
+                                console.log("Please select the manager of the employee you wish to add.");
+                                return false;
+                            }
+                        }
+                    }
+                ])
+                .then(({first_name, last_name, role_id, manager_id}) => {
+                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${first_name}", "${last_name}", ${role_id}, ${manager_id})`)
+                    console.log(`Added ${first_name} ${last_name} to the database.`)
+                    promptMenu();
+                })
+        })
+    })
+}
+
+const updateRole = () => {
     
 }
 
--- schema.sql
+++ schema.sql
@@ -4,24 +4,22 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department (id)
-    ON DELETE SET NULL,
-    PRIMARY KEY (id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
@@ -31,6 +29,5 @@ CREATE TABLE employee (
     manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employee (id)
-    ON DELETE SET NULL,
-    PRIMARY KEY (id)
+    ON DELETE SET NULL
 );
